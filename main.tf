47b01a1cc84e816da810b74e7cb2f745b36d87cc
TheTechBell@gmail.com <TheTechBell>
2023-03-12 17:08:46 +0000

    LAMP working manual only main.tf

--- main.tf
+++ main.tf
@@ -1,152 +1,176 @@
-resource "random_pet" "rg_name" {
-  prefix = var.resource_group_name_prefix
+provider "azurerm" {
+  # The "feature" block is required for AzureRM provider 2.x. 
+  # If you are using version 1.x, the "features" block is not allowed.
+  #version = "~>2.0"
+  features {}
 }
 
-resource "azurerm_resource_group" "rg" {
-  location = var.resource_group_location
-  name     = random_pet.rg_name.id
-}
+#Create the blob storage backend in Azure before running this
+#Export access key and store in ARM_ACCESS_KEY
+#This block cannot read from tfvars as this is assoicated with a provider
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-experiments-sea"
+    storage_account_name = "terraformblobstoragerag"
+    container_name       = "terraform"
+    key                  = "terraform-helming.tfstate"
 
-# Create virtual network
-resource "azurerm_virtual_network" "my_terraform_network" {
-  name                = "myVnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
-
-# Create subnet
-resource "azurerm_subnet" "my_terraform_subnet" {
-  name                 = "mySubnet"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.my_terraform_network.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-# Create public IPs
-resource "azurerm_public_ip" "my_terraform_public_ip" {
-  name                = "myPublicIP"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
-}
-
-# Create Network Security Group and rule
-resource "azurerm_network_security_group" "my_terraform_nsg" {
-  name                = "myNetworkSecurityGroup"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  security_rule {
-    name                       = "SSH"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
   }
 }
 
-# Create network interface
-resource "azurerm_network_interface" "my_terraform_nic" {
-  name                = "myNIC"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  ip_configuration {
-    name                          = "my_nic_configuration"
-    subnet_id                     = azurerm_subnet.my_terraform_subnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.my_terraform_public_ip.id
-  }
-}
-
-# Connect the security group to the network interface
-resource "azurerm_network_interface_security_group_association" "example" {
-  network_interface_id      = azurerm_network_interface.my_terraform_nic.id
-  network_security_group_id = azurerm_network_security_group.my_terraform_nsg.id
-}
-
-# Generate random text for a unique storage account name
-resource "random_id" "random_id" {
-  keepers = {
-    # Generate a new ID only when a new resource group is defined
-    resource_group = azurerm_resource_group.rg.name
-  }
-
-  byte_length = 8
-}
-
-# Create storage account for boot diagnostics
-resource "azurerm_storage_account" "my_storage_account" {
-  name                     = "diag${random_id.random_id.hex}"
-  location                 = azurerm_resource_group.rg.location
-  resource_group_name      = azurerm_resource_group.rg.name
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-# Create (and display) an SSH key
-#resource "tls_private_key" "example_ssh" {
-#  algorithm = "RSA"
-#  rsa_bits  = 4096
-#}
-
-# Create virtual machine
-resource "azurerm_linux_virtual_machine" "my_terraform_vm" {
-  name                  = "myVM"
-  location              = azurerm_resource_group.rg.location
-  resource_group_name   = azurerm_resource_group.rg.name
-  network_interface_ids = [azurerm_network_interface.my_terraform_nic.id]
-  size                  = "Standard_B2s"
-
-  os_disk {
-    name                 = "myOsDisk"
-    caching              = "ReadWrite"
-    storage_account_type = "Premium_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts-gen2"
-    version   = "latest"
-  }
-
-  computer_name                   = "myvm"
-  admin_username                  = "azureuser"
-  disable_password_authentication = true
-
-  admin_ssh_key {
-    username   = "azureuser"
-    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDJubBXlfdhKDL887+V0U2Gby1bPM8JcJVMnt5xn4zHXf2UNplHpWOyZ0SW7vru5i7wtbfukj1WvJGq8C2g44KFPL/4rJh5JpVebvn/aYxxV6Bx3DC2nk4ocFGet3keuEPzH/NUd7AtNRKwptc4JRPIVbFYhR+iBeqo3egkdB5wj44Ems6bnTnE96szXOLZCPHhpjUXpm+VCU2V8GFL3QLpsidrpP/ihcUHTxWrxuDAzbiMktEjFOhLDqD2TqIn5UkhsCL38haoSnjmaT1nv0f/gL8apyDlAD75dcrZHD925Vnishp6a6M05hdWsSz6KXzhAg2kjWmBVTtGilkTUT1z3GZSfJOvS/ODV71L5Oh7ut81JkXvcDTwrbsx3pAk4rpTTVEBq4edjpAKTT2Jghjqw+UYyE0wPzPXatDzvQUh/ANkMbvOFVdKdCsSdbfOYJtUUD47ULI/rxy8urpDbZUnziML4hJEwqEayqh584rxDvBC85rRUa/yqov9A2wr3gE= generated-by-azure"
-  }
-
-  boot_diagnostics {
-    storage_account_uri = azurerm_storage_account.my_storage_account.primary_blob_endpoint
-  }
+variable "prefix" {
+  description = "Value of the Name tag for the EC2 instance"
+  type        = string
+  default     = "raghutf"
 }
 
 
-#Create kubernetes cluster
+variable "location" {
+  description = "Value of the Name tag for the EC2 instance"
+  type        = string
+  default     = "eastus"
+}
 
-resource "azurerm_kubernetes_cluster" "cluster" {
-  name                = "testaks"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = "learnk8scluster"
+
+
+resource "azurerm_resource_group" "demo" {
+  name     = "${var.prefix}-rg"
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "demo" {
+  name                = "${var.prefix}-network"
+  location            = azurerm_resource_group.demo.location
+  resource_group_name = azurerm_resource_group.demo.name
+  address_space       = ["10.1.0.0/16"]
+}
+
+resource "azurerm_subnet" "demo" {
+  name                 = "${var.prefix}-subnet"
+  virtual_network_name = azurerm_virtual_network.demo.name
+  resource_group_name  = azurerm_resource_group.demo.name
+  address_prefixes     = ["10.1.0.0/22"]
+}
+
+resource "azurerm_kubernetes_cluster" "demo" {
+  name                = "${var.prefix}-aks"
+  location            = azurerm_resource_group.demo.location
+  resource_group_name = azurerm_resource_group.demo.name
+  dns_prefix          = "${var.prefix}-aks"
 
   default_node_pool {
-    name       = "default"
-    node_count = "2"
-    vm_size    = "standard_B2s"
+    name                = "default"
+    node_count          = 1
+    vm_size             = "Standard_B2s"
+    type                = "VirtualMachineScaleSets"
+    availability_zones  = ["1", "2"]
+    enable_auto_scaling = true
+    min_count           = 1
+    max_count           = 3
+
+    # Required for advanced networking
+    vnet_subnet_id = azurerm_subnet.demo.id
   }
 
   identity {
     type = "SystemAssigned"
   }
+
+  network_profile {
+    network_plugin    = "azure"
+    load_balancer_sku = "standard"
+    network_policy    = "calico"
+  }
+
+  tags = {
+    Environment = "Development"
+  }
+}
+
+data "terraform_remote_state" "statefile" {
+  backend = "azurerm"
+  config = {
+    resource_group_name  = "rg-experiments-sea"
+    storage_account_name = "terraformblobstoragerag"
+    container_name       = "terraform"
+    key                  = "terraform-helming.tfstate"
+
+  }
+}
+
+data "azurerm_kubernetes_cluster" "credentials" {
+  name                = azurerm_kubernetes_cluster.demo.name
+  resource_group_name = azurerm_resource_group.demo.name
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.azurerm_kubernetes_cluster.credentials.kube_config.0.host
+    client_certificate     = base64decode(data.azurerm_kubernetes_cluster.credentials.kube_config.0.client_certificate)
+    client_key             = base64decode(data.azurerm_kubernetes_cluster.credentials.kube_config.0.client_key)
+    cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.credentials.kube_config.0.cluster_ca_certificate)
+
+  }
+}
+
+resource "helm_release" "nginx_ingress" {
+  name = "nginx-ingress-controller"
+  namespace = "default"
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "nginx-ingress-controller"
+  
+  set {
+    name  = "service.type"
+    value = "LoadBalancer"
+  }
+}
+resource "helm_release" "mysql" {
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "mysql"
+  name       = "mysql"
+  version    = "9.4.8"
+  namespace  = "default"
+
+  set {
+    name  = "mysqlRootPassword"
+    value = "mysecretpassword"
+  }
+
+  set {
+    name  = "mysqlUser"
+    value = "myuser"
+  }
+
+  set {
+    name  = "mysqlPassword"
+    value = "mypassword"
+  }
+
+  set {
+    name  = "mysqlDatabase"
+    value = "mydatabase"
+  }  
+}
+
+
+
+
+resource "helm_release" "apache" {
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "apache"
+  name       = "apache"
+  version    = "9.2.11"
+  namespace  = "default"
+
+}
+
+
+resource "helm_release" "lamp" {
+  repository = "https://lead4good.github.io/lamp-helm-repository"
+  chart      = "lamp"
+  name       = "my-lamp"
+  version    = "1.1.5"
+  namespace  = "default"
+
 }
 
