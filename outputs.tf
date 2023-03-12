1fee83c95709708c44446ab82eee68e656ada6bd
TheTechBell@gmail.com <TheTechBell>
2023-03-12 08:42:45 +0000

    AKS with VM

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,19 @@
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "public_ip_address" {
+  value = azurerm_linux_virtual_machine.my_terraform_vm.public_ip_address
+}
+
+output "tls_private_key" {
+  value     = tls_private_key.example_ssh.private_key_pem
+  sensitive = true
+}
+
 resource "local_file" "kubeconfig" {
   depends_on   = [azurerm_kubernetes_cluster.cluster]
   filename     = "kubeconfig"
   content      = azurerm_kubernetes_cluster.cluster.kube_config_raw
 }
+
