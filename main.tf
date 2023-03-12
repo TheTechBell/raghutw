26a45a9a6600f10cad8679b708a7f6648a17a64b
TheTechBell@gmail.com <TheTechBell>
2023-03-12 05:48:37 +0000

    main.tf file

--- main.tf
+++ main.tf
@@ -2,18 +2,13 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=2.82.1"
+      version = ">= 2.82.1"
     }
   }
 }
 
 provider "azurerm" {
   features {}
-
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
 }
 
 resource "azurerm_resource_group" "test" {
